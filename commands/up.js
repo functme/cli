a66049ebe6ed895a7bd4b3dbcebf8744fb4e0834
Keith Horwood <keithwhor@gmail.com>
2024-07-08 23:47:32 -0400

    Update timeout

--- commands/up.js
+++ commands/up.js
@@ -204,6 +204,7 @@ class UpCommand extends Command {
         {
           name: name,
           environment: env,
+          timeout: Math.max(1, Math.min(parseInt(functJSON.timeout) || 0), 10),
           tarball: {_base64: result.toString('base64')},
           _stream: true
         },
